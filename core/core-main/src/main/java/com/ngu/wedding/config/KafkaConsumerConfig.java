22fe275598bb2fba40f01c0631280e42c82fbc61
xxspazzzxx <mxodonnell88@gmail.com>
2018-10-06 03:00:33 -0400

    Added some notes after reading and changed the topic to match domain

--- core/core-main/src/main/java/com/ngu/wedding/config/KafkaConsumerConfig.java
+++ core/core-main/src/main/java/com/ngu/wedding/config/KafkaConsumerConfig.java
@@ -12,6 +12,9 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+/**
+ * Init the Consumer Config for Kafka.  GroupID could be overarching Domain, maybe break out more configs?
+ */
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig
--- core/core-main/src/main/java/com/ngu/wedding/config/KafkaListenerConfig.java
+++ core/core-main/src/main/java/com/ngu/wedding/config/KafkaListenerConfig.java
@@ -1,17 +1,27 @@
 package com.ngu.wedding.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 
+/**
+ * Spring wired object that has a Kafka Listener.  Using weddinginfo as the topic.  GroupID is the consumer on this
+ * topic (domain) to do "something" with the payload.
+ */
 @Configuration
 //@Profile("dev")
 public class KafkaListenerConfig
 {
 
-	@KafkaListener(topics = "guests", groupId = "weddinginfo")
-	public void listen(String message) {
+	@KafkaListener(topics = "weddinginfo", groupId = "guests")
+	public void listenForGuestRequest(String message)
+	{
 		System.out.println("Received a request to attend: " + message);
+	}
+
+	@KafkaListener(topics = "weddinginfo", groupId = "hosts")
+	public void listenForHostMessage(String message)
+	{
+		System.out.println("Received a message from a host: " + message);
 	}
 
 }
