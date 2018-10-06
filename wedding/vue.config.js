fc068351f8f3ece274c1f8cfab1829b9a1678ce0
xxspazzzxx <mxodonnell88@gmail.com>
2018-10-06 01:18:39 -0400

    updating to match springBoot port

--- wedding/vue.config.js
+++ wedding/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://localhost:12345',
         ws: true,
         changeOrigin: true
       }
