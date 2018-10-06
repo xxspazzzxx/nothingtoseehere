7a683871b9b7ca71ffc86e8ed0b82e93660f4423
Brian Kennedy <briankenendy.thomsonreuters.com>
2018-10-06 01:16:50 -0400

    kafka clean up

--- core/core-main/src/main/java/com/ngu/wedding/config/KafkaConsumerConfig.java
+++ core/core-main/src/main/java/com/ngu/wedding/config/KafkaConsumerConfig.java
@@ -25,30 +25,23 @@ public class KafkaConsumerConfig
 	}
 
 	@Bean
-	public ConsumerFactory<String, String> consumerFactory() {
+	public ConsumerFactory<String, String> consumerFactory()
+	{
 		Map<String, Object> props = new HashMap<>();
-		props.put(
-				ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-				applicationProperties.getKafkaUri());
-		props.put(
-				ConsumerConfig.GROUP_ID_CONFIG,
-				applicationProperties.getKafkaGroupId());
-		props.put(
-				ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-				StringDeserializer.class);
-		props.put(
-				ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-				StringDeserializer.class);
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, applicationProperties.getKafkaUri());
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, applicationProperties.getKafkaGroupId());
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		
 		return new DefaultKafkaConsumerFactory<>(props);
 	}
 
 	@Bean
 	public ConcurrentKafkaListenerContainerFactory<String, String>
-	kafkaListenerContainerFactory() {
-
-		ConcurrentKafkaListenerContainerFactory<String, String> factory
-				= new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
+	kafkaListenerContainerFactory(ConsumerFactory<String, String> consumerFactory)
+	{
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory);
 		return factory;
 	}
 
